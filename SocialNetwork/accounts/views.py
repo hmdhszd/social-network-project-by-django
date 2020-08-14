65fd42c75a58369682790cca346b24cc1e3e048f
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-08-14 12:07:51 +0430

    Created: User model, Sign Up view, UserCreateForm

--- SocialNetwork/accounts/views.py
+++ SocialNetwork/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from . import forms
+from django.views.generic import CreateView
 # Create your views here.
+
+
+class SignUp(CreateView):
+    pass
